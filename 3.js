fc95fca25a2f428f2773c30ce6efb40b6e80df2b
EvgenStr <juve2910@gmail.com>
2018-03-11 23:11:35 +0200

    home-2

--- 3.js
+++ 3.js
@@ -1,12 +1,10 @@
 // Задание 3
 // Пользователь вводит число, нужно создать массив из чисел от 0 до "минус" число пользователя
 
-var a = parseInt(process.argv[2]);
 var mass = [];
-var len = (a*2+1);
+var len = (parseInt(process.argv[2])*2+1);
 for (var i = 0; i <= len; i++) {
-    mass[i] = a;
-    a--;
-}
+    mass[i] = parseInt(process.argv[2]-i);
+   }
 console.log(mass);
 
--- 7.js
+++ 7.js
@@ -5,12 +5,14 @@
 
 var a = parseInt(process.argv[2]);
 var mass = [1, 7, 2, 1, 2, 3, 4, 4, 9];
+var summ = "";
 for (var i = 0; i < mass.length; i++) {
   for (var k = i + 1; k < mass.length; k++) {
     if (mass[i] + mass[k] == a) {
-      console.log(
-        `Числа которые в сумме дают число ${a} это ${mass[i]} и ${mass[k]}`
-      );
+      // console.log(
+      //   `Числа которые в сумме дают число ${a} это ${mass[i]} и ${mass[k]}`
+      summ += ` ${mass[i]} и ${mass[k]}; `;
     }
   }
 }
+console.log(`Числа которые в сумме дают число ${a} это ${summ} `)
